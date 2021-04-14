cd0d0831baeefce904aae0c7bc721b03dcc5e484
Piotr Karski <pj.karski@gmail.com>
2021-04-14 15:40:57 +0200

    Odcinek 16

--- test.js
+++ test.js
@@ -1,21 +1,17 @@
-var myAge = 50;
+var myAge =26;
 
-if(myAge > 30){
+if (myAge < 18 || myAge > 30 || myAge  === 25){
 
-    document.write("you are over 30!");
+    document.write("you aint coming");
 
-}else if (myAge >20){
-
-    document.write("you are over 20!");
-
-}else if (myAge > 10){
-
-    document.write("you are over 10!");
+   
 
 }else{
 
-    document.write("you are not over 10!");
+    document.write("You can come, you cool  dude");
+
 }
+
 
 
 
