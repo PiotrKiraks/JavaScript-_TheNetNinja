bb9f099eeb4409a23ca5d2ad8550468ffd6309ca
Piotr Karski <pj.karski@gmail.com>
2021-04-14 15:24:32 +0200

    Odcinek 15

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
-var youLikeMeat = false;
-var myNumb = 10;
+var myAge = 50;
 
+if(myAge > 30){
 
-if(myNumb == 10){
+    document.write("you are over 30!");
 
-    document.write("myNumb is equal to 10");
+}else if (myAge >20){
 
-} else {
-    document.write("myNum is not equal to 10")
+    document.write("you are over 20!");
 
+}else if (myAge > 10){
+
+    document.write("you are over 10!");
+
+}else{
+
+    document.write("you are not over 10!");
 }
+
 
 
 
