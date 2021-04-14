62a304bc8adddc7371a38c3c7b2f5d3343c51ed7
Piotr Karski <pj.karski@gmail.com>
2021-04-14 15:17:01 +0200

    Lekcja if/else

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-alert("Hello, World");
+var youLikeMeat = false;
+var myNumb = 10;
+
+
+if(myNumb == 10){
+
+    document.write("myNumb is equal to 10");
+
+} else {
+    document.write("myNum is not equal to 10")
+
+}
+
+
+
+
+
+
